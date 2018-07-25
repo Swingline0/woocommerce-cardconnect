a4ca3386a5126cf9b6e237304f363798056c45bc
Eran Schoellhorn <eschoellhorn@352inc.com>
2018-07-25 07:00:11 -0400

    test: centralize purchase prep as command to use elsewhere

--- cypress/integration/basic_purchase.spec.js
+++ cypress/integration/basic_purchase.spec.js
@@ -3,39 +3,7 @@ describe('Basic Purchase', () => {
   // This block will run before every test. It ensure that there are items in the cart and
   // the custom billing info is completed. Skip to the first function `it(...` to see a test.
   beforeEach(() => {
-    cy.clearCookies();
-    // Go to shop page
-    cy.visit(`?post_type=product`);
-    cy.get('title').contains('Products – Test Site');
-    cy.wait(1000);
-    // Add each test product to cart
-    cy.get('.post-8 > .button').click();
-    cy.get('.post-9 > .button').click();
-    cy.get('.post-10 > .button').click();
-    cy.wait(1000);
-    cy.get('.post-8').contains('View cart');
-    cy.get('.post-9').contains('View cart');
-    cy.get('.post-10').contains('View cart');
-    // Go to cart page & verify items are in cart
-    cy.visit('?page_id=5');
-    cy.get('.cart_item').should('have.length', 3);
-    cy.wait(1000);
-    // Proceed to checkout and complete billing info
-    cy.visit('?page_id=6');
-    cy.get('#billing_first_name')
-      .type('{selectall}{del}Test');
-    cy.get('#billing_last_name')
-      .type('{selectall}{del}User');
-    cy.get('#billing_address_1')
-      .type('{selectall}{del}123 Fake Street');
-    cy.get('#billing_city')
-      .type('{selectall}{del}Testville');
-    cy.get('#billing_postcode')
-      .type('{selectall}{del}12345');
-    cy.get('#billing_phone')
-      .type('{selectall}{del}123-456-7890');
-    cy.get('#billing_email')
-      .type('{selectall}{del}test@example.com');
+    cy.prepareToPurchase();
   });
 
   it('Makes a purchase with a single blur', () => {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,35 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('prepareToPurchase', () => {
+  cy.clearCookies();
+  // Go to shop page
+  cy.visit(`?post_type=product`);
+  cy.get('title').contains('Products – Test Site');
+  cy.wait(1000);
+  // Add each test product to cart
+  cy.get('.post-8 > .button').click();
+  cy.get('.post-9 > .button').click();
+  cy.get('.post-10 > .button').click();
+  cy.wait(1000);
+  cy.get('.post-8').contains('View cart');
+  cy.get('.post-9').contains('View cart');
+  cy.get('.post-10').contains('View cart');
+  // Go to cart page & verify items are in cart
+  cy.visit('?page_id=5');
+  cy.get('.cart_item').should('have.length', 3);
+  cy.wait(1000);
+  // Proceed to checkout and complete billing info
+  cy.visit('?page_id=6');
+  cy.get('#billing_first_name')
+    .type('{selectall}{del}Test');
+  cy.get('#billing_last_name')
+    .type('{selectall}{del}User');
+  cy.get('#billing_address_1')
+    .type('{selectall}{del}123 Fake Street');
+  cy.get('#billing_city')
+    .type('{selectall}{del}Testville');
+  cy.get('#billing_postcode')
+    .type('{selectall}{del}12345');
+  cy.get('#billing_phone')
+    .type('{selectall}{del}123-456-7890');
+  cy.get('#billing_email')
+    .type('{selectall}{del}test@example.com');
+});
