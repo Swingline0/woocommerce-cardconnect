9a377d4bfcb6e7d224cd1c07bfaf38edc16f147d
Eran Schoellhorn <eschoellhorn@352inc.com>
2018-07-29 12:25:51 -0400

    test: write test to capture submit issue after validation error

---
+++ cypress/integration/basic_purchase_with_validation_issues.spec.js
@@ -0,0 +1,36 @@
+describe('Basic Purchases with validation errors', () => {
+
+  // This block will run before every test. It ensure that there are items in the cart and
+  // the custom billing info is completed. Skip to the first function `it(...` to see a test.
+  beforeEach(() => {
+    cy.prepareToPurchase();
+  });
+
+  it('Makes a purchase with a single blur', () => {
+
+    // Intentionally empty city field to cause validation error
+    cy.get('#billing_city')
+      .type('{selectall}{del}');
+
+    cy.get('#card_connect-card-number')
+      .type('{selectall}{del}4242424242424242');
+    cy.get('#card_connect-card-expiry')
+      .type('{selectall}{del}10/30');
+    cy.get('#card_connect-card-cvc')
+      .type('{selectall}{del}123');
+    cy.wait(3000);
+    cy.get('.card-connect-token').should('have.length', 1);
+    cy.get('#place_order').click();
+
+    cy.get('body').contains('Billing Town / City is a required field.');
+
+    // Intentionally empty city field to cause validation error
+    cy.get('#billing_city')
+      .type('{selectall}{del}Testville');
+
+    cy.get('#place_order').click();
+
+    cy.get('.woocommerce-notice').contains('Thank you. Your order has been received.');
+  });
+
+});
